285e3b8aa728f425415f0921a5e2b50c8ff885ed
matt Sullivan <matt.j.sullivan@gmail.com>
2021-01-27 14:34:04 -0800

    Separates draw render loop from play request

--- web/04_higher_level/public/javascripts/main.js
+++ web/04_higher_level/public/javascripts/main.js
@@ -19,20 +19,17 @@
         }).then((rive) => {
             // Wasm successfully loaded
             _rive = rive;
-            _processWasmLoadQueue();
+            // Fire all the callbacks
+            while (_wasmLoadQueue.length > 0) {
+                _wasmLoadQueue.shift()(_rive);
+            }
         }).catch((e) => {
             console.error('Unable to load Wasm module');
             throw e;
         });
     };
 
-    var _processWasmLoadQueue = function() {
-        while (_wasmLoadQueue.length > 0) {
-            _wasmLoadQueue.shift()(_rive);
-        }
-    };
-
-    // Add a listener for Wasm loaded
+    // Adds a listener for Wasm load
     var _onWasmLoaded = function(cb) {
         if (_rive !== undefined) {
             // Wasm already loaded, fire immediately
@@ -45,6 +42,7 @@
         }
     }
 
+    // Start loading the Wasm immediately
     _loadWasm();
 
     // Loop types. The index of the type is the value that comes from Wasm
@@ -85,13 +83,12 @@
 
         // List of animations that should be played.
         if (!animations) {
-            self._animations = null;
+            self._animationNames = null;
         } else if (typeof animations === 'string') {
-            self._animations = [animations];
+            self._animationNames = [animations];
         } else if (animations.constructor === Array) {
-            self._animations = animations;
+            self._animationNames = animations;
         }
-        
         
         self._canvas = canvas;
         self._autoplay = autoplay;
@@ -147,7 +144,9 @@
 
     RiveAnimation.prototype = {
 
-        // Callback when Wasm bundle is loaded
+        /* 
+         * Callback when Wasm bundle is loaded
+         */
         _wasmLoadEvent: function (rive) {
             var self = this;
 
@@ -155,7 +154,9 @@
              self._loadRiveFile();
         },
 
-        // Loads a Rive file
+        /*
+         * Loads a Rive file
+         */
         _loadRiveFile: function () {
             var self = this;
 
@@ -170,6 +171,17 @@
                 // Fire the 'load' event and trigger the task queue
                 if (self._file) {
                     self._loaded = true;
+
+                    // Initialize playback and paint first frame; do this here
+                    // so that if play() has already beren called, things are
+                    // initialized before we start firing loaded events
+                    self._initializePlayback();
+
+                    // Paint the first frame
+                    self._paintFrame();
+
+                    // Emit the load event, which will also kick off processing
+                    // the load queue
                     self._emit('load', 'File ' + self._src + ' loaded');
                 }
             }).catch((e) => {
@@ -177,27 +189,6 @@
                 console.error('Unable to load Rive file: ' + self._src);
                 throw e;
             });
-        },
-
-        // Initializes artboard, animations, etc. prior to playback
-        _initializePlayback: function () {
-            const self = this;
-
-            // Get the artboard that contains the animations you want to play.
-            // You animate the artboard, using animations it contains.
-            self._artboard = self._artboardName ?
-                self._file.artboard(self._artboardName) :
-                self._file.defaultArtboard();
-
-            // Check that the artboard has at least 1 animation
-            if (self._artboard.animationCount() < 1) {
-                self._emit('loaderror', 'Artboard has no animations');
-                throw 'Artboard has no animations';
-            }
-
-            // Get the canvas where you want to render the animation and create a renderer
-            self._ctx = self._canvas.getContext('2d');
-            self._renderer = new self._rive.CanvasRenderer(self._ctx);
         },
 
         /*
@@ -250,6 +241,154 @@
             return self;
         },
 
+        /*
+         * Initializes artboard, animations, etc. prior to playback
+         */
+        _initializePlayback: function () {
+            const self = this;
+
+            // Get the artboard that contains the animations you want to play.
+            // You animate the artboard, using animations it contains.
+            self._artboard = self._artboardName ?
+                self._file.artboard(self._artboardName) :
+                self._file.defaultArtboard();
+
+            // Check that the artboard has at least 1 animation
+            if (self._artboard.animationCount() < 1) {
+                self._emit('loaderror', 'Artboard has no animations');
+                throw 'Artboard has no animations';
+            }
+
+            // Get the canvas where you want to render the animation and create a renderer
+            self._ctx = self._canvas.getContext('2d');
+            self._renderer = new self._rive.CanvasRenderer(self._ctx);
+
+            // Get the animation and instance them
+            if (!self._animationNames) {
+                // No animations given, use the first one
+                self._animations = [self._artboard.animationAt(0)];
+            } else {
+                self._animations = self._animationNames.map(name => self._artboard.animation(name));
+            }
+
+            self._instances = self._animations.map(a => new self._rive.LinearAnimationInstance(a));
+
+
+            // Tracks the loop states of all animations
+            self._loopCounts = [];
+            for (var i=0; i< self._instances.length; i++) {
+                self._loopCounts.push(0);
+            }
+        },
+
+        /*
+         * Paints the first frame on the animation
+         */
+        _paintFrame: function () {    
+            const self = this;
+
+            // Choose how you want the animation to align in the canvas
+            self._ctx.save();
+            self._renderer.align(self._rive.Fit.contain, self._rive.Alignment.center, {
+                minX: 0,
+                minY: 0,
+                maxX: self._canvas.width,
+                maxY: self._canvas.height
+            }, self._artboard.bounds);
+    
+            // Advance to the first frame and draw the artboard
+            self._artboard.advance(0);
+            self._artboard.draw(self._renderer);
+            self._ctx.restore();
+        },
+
+        /*
+         * The draw rendering loop
+         */
+        _draw: function(time) {
+            const self = this;
+
+            // This is the looping function where the animation frames will be
+            // rendered at the correct time interval
+
+            // On the first pass, make sure lastTime has a valid value
+            if (!self._lastTime) {
+                self._lastTime = time;
+            }
+            // Calculate the elapsed time between frames in seconds
+            const elapsedTime = (time - self._lastTime) / 1000;
+            self._lastTime = time;
+
+            // Advance the animation by the elapsed number of seconds
+            for (const i in self._instances) {
+                self._instances[i].advance(elapsedTime);
+                if (self._instances[i].didLoop) {
+                    self._loopCounts[i] += 1;
+                }
+                // Apply the animation to the artboard. The reason of this is that
+                // multiple animations may be applied to an artboard, which will
+                // then mix those animations together.
+                self._instances[i].apply(self._artboard, 1.0);
+            }
+
+            // Once the animations have been applied to the artboard, advance it
+            // by the elapsed time.
+            self._artboard.advance(elapsedTime);
+
+            // Clear the current frame of the canvas
+            self._ctx.clearRect(0, 0, self._canvas.width, self._canvas.height);
+            // Render the frame in the canvas
+            self._ctx.save();
+            self._renderer.align(self._rive.Fit.contain, self._rive.Alignment.center, {
+                minX: 0,
+                minY: 0,
+                maxX: self._canvas.width,
+                maxY: self._canvas.height
+            }, self._artboard.bounds);
+            self._artboard.draw(self._renderer);
+            self._ctx.restore();
+
+            for (var i in self._animations) {
+                // Emit if the animation looped
+                switch (self._animations[i].loopValue) {
+                    case 0:
+                        // Do nothing; this never loops
+                        break;
+                    case 1:
+                        if (self._loopCounts[i]) {
+                            self._emit('loop', new LoopEvent({
+                                animationName: self._animations[i].name,
+                                loopValue: self._animations[i].loopValue
+                            }));
+                            self._loopCounts[i] = 0;
+                        }
+                        break;
+                    case 2:
+                        // Wasm indicates a loop at each time the animation
+                        // changes direction, so a full loop/lap occurs every
+                        // two didLoops
+                        if (self._loopCounts[i] > 1) {
+                            self._emit('loop', new LoopEvent({
+                                animationName: self._animations[i].name,
+                                loopValue: self._animations[i].loopValue
+                            }));
+                            self._loopCounts[i] = 0;
+                        }
+                        break;
+                }
+            }
+
+            // Calling requestAnimationFrame will call the draw function again
+            // at the correct refresh rate. See
+            // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_animations
+            // for more details.
+            requestAnimationFrame(self._draw.bind(self));
+    
+        },
+
+        /*
+         * Registers a callback for a named event
+         */
         on: function (event, fn) {
             var self = this;
             var events = self['_on' + event];
@@ -261,6 +400,9 @@
             return self;
         },
 
+        /*
+         * Starts/continues playback
+         */
         play: function () {
             const self = this;
 
@@ -274,117 +416,25 @@
                 return;
             }
 
-            self._initializePlayback();
-
-            // Get the animation and instance them
-            var animations;
-            if (!self._animations) {
-                // No animations given, use the first one
-                animations = [self._artboard.animationAt(0)];
-            } else {
-                animations = self._animations.map(name => self._artboard.animation(name));
-            }
-
-            const instances = animations.map(a => new self._rive.LinearAnimationInstance(a));
-
-            // Track the last time the loop was performed
-            let lastTime = 0;
-
-            // Tracks the animation time for detecting looping
-            let lastAnimationTime = 0;
-
-            // Tracks the loop states of all animations
-            var loopCounts = [];
-            instances.forEach(_ => loopCounts.push(0));
-
-            // This is the looping function where the animation frames will be
-            // rendered at the correct time interval
-            function draw(time) {
-                // On the first pass, make sure lastTime has a valid value
-                if (!lastTime) {
-                    lastTime = time;
-                }
-                // Calculate the elapsed time between frames in seconds
-                const elapsedTime = (time - lastTime) / 1000;
-                lastTime = time;
-
-                // Advance the animation by the elapsed number of seconds
-                for (const i in instances) {
-                    instances[i].advance(elapsedTime);
-                    if (instances[i].didLoop) {
-                        loopCounts[i] += 1;
-                    }
-                    // Apply the animation to the artboard. The reason of this is that
-                    // multiple animations may be applied to an artboard, which will
-                    // then mix those animations together.
-                    instances[i].apply(self._artboard, 1.0);
-               }
-
-                // Once the animations have been applied to the artboard, advance it
-                // by the elapsed time.
-                self._artboard.advance(elapsedTime);
-
-                // Clear the current frame of the canvas
-                self._ctx.clearRect(0, 0, self._canvas.width, self._canvas.height);
-                // Render the frame in the canvas
-                self._ctx.save();
-                self._renderer.align(self._rive.Fit.contain, self._rive.Alignment.center, {
-                    minX: 0,
-                    minY: 0,
-                    maxX: self._canvas.width,
-                    maxY: self._canvas.height
-                }, self._artboard.bounds);
-                self._artboard.draw(self._renderer);
-                self._ctx.restore();
-
-                for (var i in animations) {
-                    // Emit if the animation looped
-                    switch (animations[i].loopValue) {
-                        case 0:
-                            // Do nothing; this never loops
-                            break;
-                        case 1:
-                            if (loopCounts[i]) {
-                                self._emit('loop', new LoopEvent({
-                                    animationName: animations[i].name,
-                                    loopValue: animations[i].loopValue
-                                }));
-                                loopCounts[i] = 0;
-                            }
-                            break;
-                        case 2:
-                            // Wasm indicates a loop at each time the animation
-                            // changes direction, so a full loop/lap occurs every
-                            // two didLoops
-                            if (loopCounts[i] > 1) {
-                                self._emit('loop', new LoopEvent({
-                                    animationName: animations[i].name,
-                                    loopValue: animations[i].loopValue
-                                }));
-                                loopCounts[i] = 0;
-                            }
-                            break;
-                    }
-                }
-
-                // Calling requestAnimationFrame will call the draw function again
-                // at the correct refresh rate. See
-                // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_animations
-                // for more details.
-                requestAnimationFrame(draw)
-            }
-
             // Start animating by calling draw on the next refresh cycle.
-            requestAnimationFrame(draw);
+            requestAnimationFrame(self._draw.bind(self));
 
             // Emit a play event
-            self._emit('play', 'Playing ' + self._animation);
-        }
+            const msg = 'Playing: ' + self._animationNames.join(', ');
+            self._emit('play', msg);
+        },
 
+        /*
+         * Pauses playback
+         */
+        pause: function() {},
     };
 
-    // Handy debugging function to print contents of js object; for
-    // debugging purposes only
+    /*
+     * Utilities and debugging code below
+     */ 
+
+    // Handy debugging function to print contents of js object
     function printProps(obj) {
         var propValue;
         for (var propName in obj) {
@@ -407,10 +457,10 @@
         // onloop: (l) => { console.log('Loop: ' + l.animationName + ': ' + l.loopName); },
     });
 
-    // // Will start the animation once the animation is loaded
+    // Will start the animation once the animation is loaded
     // anim.play();
 
-    // // // Subscribe to listen to events
+    // Subscribe to listen to events
     // anim.on('load', () => {
     //     console.log('External detected load');
     // });
